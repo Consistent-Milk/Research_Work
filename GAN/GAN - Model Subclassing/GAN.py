601df02059e6e5a727fb2648bd1a652ac7d69ae8
Consistent Milk <mahmudulhasanfh91@gmail.com>
2022-07-22 15:12:02 +0600

    22th July

--- GAN/GAN - Model Subclassing/GAN.py
+++ GAN/GAN - Model Subclassing/GAN.py
@@ -1,11 +1,3 @@
 import tensorflow as tf
 from tensorflow import keras
 
-class GAN(keras.Model):
-
-    def __init__(self, discriminator, generator, latent_dim):
-        super.__init__()
-        self.discriminator = discriminator
-        self.generator = generator
-        self.latent_dim = latent_dim
-        self.d_loss_metric = keras.metrics.Mean()
